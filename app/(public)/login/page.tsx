446180712bac3852270d2d9686ef3833f5a4ae9e
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-01-15 19:55:28 +0530

    feat: implement authentication and secure routes with login functionality

---
+++ app/(public)/login/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { setToken } from "@/utils/auth";
+import { ENDPOINTS } from "@/utils/apiConfig";
+
+export default function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(ENDPOINTS.LOGIN, {
+        email,
+        password,
+      });
+      setToken(res.data.token);
+      router.push("/user");
+    } catch {
+      setError("Invalid credentials");
+    }
+  };
+  return (
+    <div className="h-full min-h-screen w-screen bg-black text-white flex items-center justify-center">
+      <div className="border border-white/20 p-8 rounded-lg w-full max-w-sm">
+        <h1 className="text-3xl font-bold text-white/80 mb-6">Login</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-2 mb-4 text-white/80 bg-white/10 rounded-md border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-2 mb-4 text-white/80 bg-white/10 rounded-md border border-white/20 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          onClick={handleLogin}
+          className="w-full py-2 text-black bg-white rounded-md hover:bg-gray-100 duration-300"
+        >
+          Login
+        </button>
+        {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+      </div>
+    </div>
+  );
+}
---
+++ utils/apiConfig.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://localhost:8080";
+
+export const ENDPOINTS = {
+  LOGIN: `${BASE_URL}/auth/login`,
+  SECURE: `${BASE_URL}/secure`,
+};
